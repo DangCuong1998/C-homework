a0a0e15a0129bd89a3e9cd9c4833250e3d603d9a
DangCuong <cuongtrieuphu98@gmail.com>
2022-05-09 10:02:22 +0700

    day bai 1.3

---
+++ 1.3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	int b;
+	float thuong;
+	cout<<"nhap so a:";
+	cin>>a;
+	cout<<"nhap so b:";
+	cin>>b;
+	cout<<"Tong hai so:"<<a+b<<endl;
+	cout<<"hieu hai so:"<<a-b<<endl;
+	cout<<"tich hai so: "<<a*b<<endl;
+	thuong = float(a)/float(b);
+	cout<<"thuong hai so:"<<thuong;
+}
